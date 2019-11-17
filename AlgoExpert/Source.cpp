19bf01486e8c82f5d001341a71d18185882c1e1d
RogerVDW <rogerscode@gmail.com>
2019-11-17 14:23:00 +0100

    Added non recursive solution (buildinig dynamic profit table)

--- AlgoExpert/Source.cpp
+++ AlgoExpert/Source.cpp
@@ -66,15 +66,54 @@ int maxProfitWithKTransactions(std::vector<int> prices, int k)
 	}
 }
 
-// SOLUTION 2: AlgoExpert solution
-
+// SOLUTION 2: AlgoExpert solution; build a profit table size (t=k)x(d=prices.size()) that contains max achievable profit for each cell [t][d]
+int maxProfitWithKTransactions2(std::vector<int> prices, int k)
+{
+	if (prices.size() < 2)
+	{
+		return 0;
+	}
+	else
+	{
+		std::vector<std::vector<int>> profit(k + 1, std::vector<int>(prices.size()));
+		for (int i = 1; i < k + 1; i++)
+		{
+			for (int j = 1; j < prices.size(); j++)
+			{
+				int val1 = profit[i][j - 1];
+				int val_t = (-prices[0] + profit[i - 1][0]);
+				for (int x = 1; x < j; x++)
+				{
+					int p = (-prices[x] + profit[i - 1][x]);
+					if (p > val_t) val_t = p;
+				}
+				int val2 = val_t + prices[j];
+				profit[i][j] = std::max(val1, val2);
+			}
+		}
+		return profit[k][prices.size() - 1];
+	}
+}
 
 int main()
 {
+	//std::vector<std::vector<int>> table0(2);
+	//table0.emplace_back(std::vector<int>{2});
+	//table0.emplace_back(std::vector<int>{ 1,1 });
+	//table0[0] = std::vector<int>{ 10,10,10,10 };
+
+	//std::vector<std::vector<int>> table(2,std::vector<int>(4));
+	//table[1][3] = 10;
+	//int a = table[1][3];
+	//
+	//int arr[3][2] = { {1,2},{3,4},{5,6} };
+	//int b = arr[2][1];
+	
+	
 	//std::vector<int> prices = { 1,100,101,200,201,300,301,400,401,500 };
-	//std::vector<int> prices = { 5,11,3,50,60,90 };
-	std::vector<int> prices = { 1,10 };
-	int max_profit = maxProfitWithKTransactions(prices, 3);
+	std::vector<int> prices = { 5,11,3,50,60,90 };
+	//std::vector<int> prices = { 1,10 };
+	int max_profit = maxProfitWithKTransactions2(prices, 2);
 	std::cout << "Max profit is: " << max_profit;
 
 	//std::vector<int> prices = { 1,2,3,4,5 };
